9df1bb0ef5af933d59625e8c752ce4d5ff136705
dschaym <dschay@gmail.com>
2026-02-22 00:36:16 -0600

    Creacion de validaciones

--- ChayDiana_Ejercicio3/Program.cs
+++ ChayDiana_Ejercicio3/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("SISTEMA DE FACTURACION CON CONTROL ANTIFRAUDE");
+﻿using System.Globalization;
+
+Console.WriteLine("SISTEMA DE FACTURACION CON CONTROL ANTIFRAUDE");
 Console.WriteLine("Tipo de cliente");
 Console.WriteLine("1. Estudiante");
 Console.WriteLine("2. Docente");
@@ -29,3 +31,34 @@ Console.WriteLine("2) Cupón inválido repetido");
 Console.WriteLine("3) Pagos rechazados múltiples");
 Console.Write("Seleccione (1-3): ");
 int reporte = int.Parse(Console.ReadLine());
+
+if (tipoCliente < 1 || tipoCliente > 4)
+        {
+            Console.WriteLine("Error: Tipo de cliente fuera de rango (1-4).");
+            return;
+        }
+        else if (montoBase < 0)
+        {
+            Console.WriteLine("Error: El monto base no puede ser negativo.");
+            return;
+        }
+        else if (metodo < 1 || metodo > 3)
+        {
+            Console.WriteLine("Error: Método de pago fuera de rango (1-3).");
+            return;
+        }
+        else if (tieneCupon != "S" && tieneCupon != "N")
+        {
+            Console.WriteLine("Error: 'Tiene cupón' debe ser S o N.");
+            return;
+        }
+        else if (tieneCupon == "S" && string.IsNullOrWhiteSpace(codigoCupon))
+        {
+            Console.WriteLine("Error: Indicó que tiene cupón, pero no ingresó el código.");
+            return;
+        }
+        else if (reporte < 1 || reporte > 3)
+        {
+            Console.WriteLine("Error: Reporte antifraude fuera de rango (1-3).");
+            return;
+        }
